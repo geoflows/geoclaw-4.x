16eef30f107ecc666069f893db8be9631f0dd8d6
KYANJO <brian.kyanjo@aims.ac.rw>
2023-03-11 13:30:47 -0700

    commented out deprecatted issues

--- geoclaw/2d/lib/setprob_geo.f
+++ geoclaw/2d/lib/setprob_geo.f
@@ -13,14 +13,14 @@ c=========================================================================
 
       call set_geo          !# sets basic parameters g and coord system
       call set_tsunami      !# sets parameters specific to tsunamis
-      call set_flow_grades  !# sets refinement parameters
+c     call set_flow_grades  !# sets refinement parameters
       call set_qinit        !# specifies file(s) for initial conditions
-      call set_auxinit      !# specifies file(s) for auxiliary variables
+c     call set_auxinit      !# specifies file(s) for auxiliary variables
       call set_topo         !# specifies topography (bathymetry) files
-      call set_dtopo        !# specifies moving topo 'dtopo' from earthquake
+c     call set_dtopo        !# specifies moving topo 'dtopo' from earthquake
       call setregions       !# specifies where refinement is allowed/forced
       call setgauges        !# locations of measuring gauges
-      call setfixedgrids    !# specifies output on arbitrary uniform fixed grids
+c     call setfixedgrids    !# specifies output on arbitrary uniform fixed grids
 
       return
       end
