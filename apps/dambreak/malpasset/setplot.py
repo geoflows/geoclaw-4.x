bf78bdcd0f07ca1fd8a478aff893a17480b89194
KYANJO <brian.kyanjo@aims.ac.rw>
2023-03-12 05:40:32 -0600

    Malpasset problem working

--- apps/dambreak/malpasset/setplot.py
+++ apps/dambreak/malpasset/setplot.py
@@ -7,14 +7,8 @@ function setplot is called to set the plot parameters.
 
 """
 
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import image
-from clawpack.geoclaw import topotools
-
-# --------------------- Police, transformer and guage data -----------------------------------------------
-malpasset_loc = "./malpasset_locs.txt"
+from pyclaw.geotools import topotools
+from pyclaw.data import Data
 
 
 #--------------------------
@@ -29,201 +23,74 @@ def setplot(plotdata):
     """
 
 
-    from clawpack.visclaw import colormaps, geoplot
+    from pyclaw.plotters import colormaps, geoplot
     from numpy import linspace
 
     plotdata.clearfigures()  # clear any old figures,axes,items data
-    plotdata.format = "forestclaw"
 
-    plotdata.verbose = False
-
-
-    #-----------------------------------------
-    # Some global kml flags
-    #-----------------------------------------
-    plotdata.kml_name = "Malpasset Dam"
-    plotdata.kml_starttime = [1959,12,2,21,14,0]  # Date/time of event in UTC [None]
-    plotdata.kml_tz_offset = 1    # Time zone offset (in hours) of event. [None]
-
-    plotdata.kml_index_fname = "MalpassetDam"  # name for .kmz and .kml files ["_GoogleEarth"]
-
-    # Set to a URL where KMZ file will be published.
-    # plotdata.kml_publish = 'http://math.boisestate.edu/~calhoun/visclaw/GoogleEarth/kmz'
-
-    # Add [file_name,visibility]
-    # ----commented out
-    # plotdata.kml_user_files.append(['malpasset_dam_validate.kml',True])
-
-    # Cells used in setrun.py (original)
-    num_cells = [54,19]
     lower = [953236.00000000, 1832407.25000000]
     upper = [959554.00000000, 1848572.75000000]
 
-    # lower = [6.69262004,  43.40252126]
-    # upper = [6.78171094,  43.55083988 ]
-    #-----------------------------------------------------------
-    # Figure for KML files (large view)
-    # This under-resolves the finest level.
-    #----------------------------------------------------------
-    plotfigure = plotdata.new_plotfigure(name='Malpasset Dam',figno=1)
+    # To plot gauge locations on pcolor or contour plot, use this as
+    # an afteraxis function:
+
+    def addgauges(current_data):
+        from pyclaw.plotters import gaugetools
+        gaugetools.plot_gauge_locations(current_data.plotdata, \
+             gaugenos='all', format_string='ko', add_labels=True)
+
+
+    figkwargs = dict(figsize=(18,12),dpi=1200)
+    #-----------------------------------------
+    # Figure for pcolor plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='pcolor', figno=1)
     plotfigure.show = True
+    plotfigure.kwargs = figkwargs
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('pcolor')
+    plotaxes.title = 'Surface'
+    plotaxes.scaled = True
 
-    plotfigure.use_for_kml = True
-    plotfigure.kml_use_for_initial_view = True
-
-    # Latlong box used for GoogleEarth
-
-    plotfigure.kml_xlimits = [lower[0], upper[0]]
-    plotfigure.kml_ylimits = [lower[1], upper[1]]
-
-    # Use computational coordinates for plotting
-    plotfigure.kml_use_figure_limits = True
-    plotfigure.kml_tile_images = False    # Tile images for faster loading.  Requires GDAL [False]
-
-
-    # --------------------------------------------------
-    # Resolution (should be consistent with data)
-    # Refinement levels : [2,4,4,4]; max level = 5; num_cells = [55,24]
-    # Aim for 1 pixel per finest level grid cell.
-    # Choose a figure size that matches the coarse grid resolution.
-    # Set the dpi so that figsize*dpi = finest level effective resolution.
-
-    # If amr refinement ratios set to [0,6]; max_level = 6
-    # figsize*dpi = [2,1]*16*2**6 = [2048,1024]
-    mx = 16
-    mi = 1
-    mj = 1
-    minlevel = 0
-    maxlevel = 4
-    p = 1
-    plotfigure.kml_figsize = [32,32]  #[mx*2**p*mi,mx*2**p*mj]
-    plotfigure.kml_dpi = 64
-
-    # --------------------------------------------------
+    def fixup(current_data):
+        import pylab
+        #addgauges(current_data)
+        t = current_data.t
+        t = t / 3600.  # hours
+        pylab.title('Surface at %4.2f hours' % t, fontsize=20)
+        pylab.xticks(fontsize=15)
+        pylab.yticks(fontsize=15)
+    plotaxes.afteraxes = fixup
 
     # Color axis : transparency below 0.1*(cmax-cmin)
     cmin = 0
     cmax = 5
-    cmap = geoplot.googleearth_flooding  # transparent --> light blue --> dark blue
-
-    # Water
-    plotaxes = plotfigure.new_plotaxes('kml')
-    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.plot_var = geoplot.depth   # Plot height field h.
-    plotitem.pcolor_cmap = geoplot.googleearth_flooding
-    plotitem.pcolor_cmin = cmin
-    plotitem.pcolor_cmax = cmax
-    # plotitem.amr_celledges_show = [0,0,0]
-    plotitem.patchedges_show = True       # Show patch edges
-
-    def kml_colorbar(filename):
-        geoplot.kml_build_colorbar(filename,cmap,cmin,cmax)
-
-    plotfigure.kml_colorbar = kml_colorbar
-
-
-    #-----------------------------------------------------------
-    # Figure for KML files (zoomed view on region)
-    #----------------------------------------------------------
-    plotfigure = plotdata.new_plotfigure(name='Transformers (zoom)',figno=2)
-    plotfigure.show = True
-
-    plotfigure.use_for_kml = True
-    plotfigure.kml_use_for_initial_view = False
-
-    # Latlong box used for GoogleEarth
-
-    import tools
-    #region_lower, region_upper, figsize = tools.region_coords(xll,xur,
-    #                                                          num_cells,
-    #                                                          lower,
-    #                                                          upper)
-
-    # # Get degrees per finest level :
-    # dx_coarse = (upper[0]-lower[0])/num_cells[0]
-    # dy_coarse = (upper[1]-lower[1])/num_cells[1]
-    #
-    # # Zoom region
-    # mx_xlow = np.floor((xll[0] - lower[0])/dx_coarse).astype(int)
-    # my_ylow = np.floor((xll[1] - lower[1])/dy_coarse).astype(int)
-    # mx_zoom = np.ceil((xur[0] - xll[0])/dx_coarse).astype(int)
-    # my_zoom = np.ceil((xur[1] - xll[1])/dy_coarse).astype(int)
-    # xlow = lower[0] + mx_xlow*dx_coarse
-    # ylow = lower[1] + my_ylow*dy_coarse
-
-    #plotfigure.kml_xlimits = [region_lower[0],region_upper[0]]
-    #plotfigure.kml_ylimits = [region_lower[1], region_upper[1]]
-
-    # Use computational coordinates for plotting
-    plotfigure.kml_use_figure_limits = True
-
-    # --------------------------------------------------
-    # plotfigure.kml_figsize = figsize*8
-    # plotfigure.kml_dpi = 4*4
-
-    # --------------------------------------------------
-
-    plotfigure.kml_tile_images = False    # Tile images for faster loading.  Requires GDAL [False]
-
-    # Color axis : transparency below 0.1*(cmax-cmin)
-    # cmin = 0
-    # cmax = 5
     # cmap = geoplot.googleearth_flooding  # transparent --> light blue --> dark blue
 
-    # # Water
-    # plotaxes = plotfigure.new_plotaxes('kml')
-    # plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    # plotitem.plot_var = geoplot.depth   # Plot height field h.
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
+    #plotitem.plot_var = geoplot.surface
+    geoplot.depth   # Plot height field h.
     # plotitem.pcolor_cmap = geoplot.googleearth_flooding
-    # plotitem.pcolor_cmin = cmin
-    # plotitem.pcolor_cmax = cmax
+    # plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.pcolor_cmap = geoplot.tsunami_colormap
+    plotitem.pcolor_cmin = cmin
+    plotitem.pcolor_cmax = cmax
+    plotitem.add_colorbar = True
+    plotitem.amr_gridlines_show = [1,0,0,0,0]
+    plotitem.gridedges_show = 1
 
-    # plot point locations 
-    police, transformers, gauges = tools.read_locations_data(malpasset_loc)
-    
-    # Physical model points locations (Gauges)
-    gauge_points = ['P6','P7','P8','P9','P10','P11','P12','P13','P14']
-    gauge_loc = []
-    for i in range(len(gauges[0])):
-        gauge_loc.append([gauges[1][i],gauges[2][i]])   
-
-    # Physical model points locations (Transformers)
-    transformer_points = ['A','B','c']
-    transformer_loc = []
-    for i in range(len(transformers[0])):
-        transformer_loc.append([transformers[1][i],transformers[2][i]])
-
-    # Physical model points locations (Police)
-    police_points = ['S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','S11','S12','S13','S14','S15','S16','S17']
-    police_loc = []
-    for i in range(len(police[0])):
-        police_loc.append([police[1][i],police[2][i]])
-
-    point_labels = transformer_points + gauge_points + police_points
-    point_list = [transformer_loc, gauge_loc, police_loc]
-    point_list = [item for sublist in point_list for item in sublist]
-    
-    # Plot and overlay points on the plotted image
-    plt.style.use('default')
-    # im = image.imread("_plots/temp/fig1/frame0098fig1/frame0098fig1.png")
-    
-    for i,pt in enumerate(point_list):
-        lab = point_labels[i]
-        if i>2 and i<=11:
-            plt.plot(pt[0],pt[1],'k*',markersize=4)
-            plt.annotate(lab, xy=pt, xytext=(pt[0]-50,pt[1]-350))
-        elif i>11:
-            plt.plot(pt[0],pt[1],'k*',markersize=4)
-            plt.annotate(lab, xy=pt, xytext=(pt[0]-25,pt[1]+100))
-        else:
-            plt.plot(pt[0],pt[1],'r*',markersize=4)
-            plt.annotate(lab, xy=pt, xytext=(pt[0]-5,pt[1]+100))
-    
-    # plt.imshow(im)
-    plt.savefig('malpasset_points.png',dpi=300)
-    
-
-    #-----------------------------------------
+    # Land
+    # plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
+    # plotitem.plot_var = geoplot.land
+    # plotitem.pcolor_cmap = geoplot.land_colors
+    # plotitem.pcolor_cmin = 0.0
+    # plotitem.pcolor_cmax = 100.0
+    # plotitem.add_colorbar = False
+    # plotitem.amr_gridlines_show = [1,0,0,0,0]
+    # plotitem.gridedges_show = 1
+    # plotaxes.xlimits = [-185,-65]
+    # plotaxes.ylimits = [-60,40]
 
     #-----------------------------------------
     # Figures for gauges
@@ -256,28 +123,27 @@ def setplot(plotdata):
     plotitem.plot_var = gaugetopo
     plotitem.plotstyle = 'g-'
 
+    # plot point locations 
+    malpasset_loc = "./malpasset_locs.txt"
+    import tools
+    police, transformers, gauges, all_guages = tools.read_locations_data(malpasset_loc)
+    
+    # Physical model points locations (Gauges)
+    gauge_points = ['P6','P7','P8','P9','P10','P11','P12','P13','P14']
+    # Physical model points locations (Transformers)
+    transformer_points = ['A','B','c']
+    # Physical model points locations (Police)
+    police_points = ['S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','S11','S12','S13','S14','S15','S16','S17']
+
     def afterframe(current_data):
         from pylab import plot, legend, xticks, floor, axis, xlabel,title
         t = current_data.t
         gaugeno = current_data.gaugeno
-        if gaugeno == 6:
-            title('P6')
-        elif gaugeno == 7:
-            title('P7')
-        elif gaugeno == 8:
-            title('P8')
-        elif gaugeno == 9:
-            title('P9')
-        elif gaugeno == 10:
-            title('P10')
-        elif gaugeno == 11:
-            title('P11')
-        elif gaugeno == 12:
-            title('P12')
-        elif gaugeno == 13:
-            title('P13')
-        elif gaugeno == 14:
-            title('P14')
+        # locations points
+        guage_labels = police_points + transformer_points + gauge_points
+        for n,pt in enumerate(guage_labels):
+            if gaugeno == n:
+                title(pt)
 
         # plot(t, 0*t, 'k')
         n = int(floor(t.max()/3600.) + 2)
@@ -286,33 +152,23 @@ def setplot(plotdata):
 
     plotaxes.afteraxes = afterframe
 
-    
+
+    #-----------------------------------------
 
     # Parameters used only when creating html and/or latex hardcopy
     # e.g., via pyclaw.plotters.frametools.printframes:
 
-    plotdata.parallel = False
-    plotdata.print_format = 'png'           # file format
-    plotdata.print_framenos = range(0,100,2)         # list of frames to print
-    plotdata.print_gaugenos = 'all'         # list of gauges to print
-    plotdata.print_fignos = [1,300]         # list of figures to print
-
-    plotdata.printfigs = True              # print figures
-    plotdata.overwrite = True
-
+    plotdata.printfigs = True                # print figures
+    plotdata.print_format = 'png'            # file format
+    plotdata.print_framenos = 'all'          # list of frames to print
+    plotdata.print_gaugenos = 'all'        # list of gauges to print
+    plotdata.print_fignos = 'all'            # list of figures to print
     plotdata.html = True                     # create html files of plots?
-    plotdata.html_movie = True                    # create html files of plots?
     plotdata.html_homelink = '../README.html'   # pointer for top of index
-
-    plotdata.latex = False                    # create latex file of plots?
-    #plotdata.latex_figsperline = 2           # layout of plots
-    #plotdata.latex_framesperline = 1         # layout of plots
-    #plotdata.latex_makepdf = False           # also run pdflatex?
-
-    plotdata.kml = True
+    plotdata.latex = True                    # create latex file of plots?
+    plotdata.latex_figsperline = 2           # layout of plots
+    plotdata.latex_framesperline = 1         # layout of plots
+    plotdata.latex_makepdf = False           # also run pdflatex?
 
     return plotdata
 
-if __name__=="__main__":
-    from clawpack.visclaw.plotclaw import plotclaw
-    plotclaw(outdir='.',setplot=setplot,plotdir='_plots',format='forestclaw')    
